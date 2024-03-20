78828a7679f9899169198ea8092ffa36ba1ad728
Amelia Vega <133181467+AmeliaVegaa@users.noreply.github.com>
2024-03-20 21:33:06 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,23 @@
+
+<!-- <div class="card">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div> -->
+ 
+<footer class="fixed-bottom">
+    <div style="background-color: #997950; color: white"  >
+  <div class="container-md d-flex justify-content-center" >
+    <a style="height: 100px;" class="navbar-brand" ></a>
+    <h1 style="margin-top: 35px; font-size: 20px;">Copyright &#169; 2024 Kelompok 8 Penugasan Pemrograman Web</h1>
+  </div>
+    </div>
+   
+</footer>
+</body>
+</html>
